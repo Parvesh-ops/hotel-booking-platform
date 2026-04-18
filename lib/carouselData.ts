5b7af0dce44d4aa12eca8edb09e113a17425483c
Parvesh <arveshchaudhary@gmail.com>
2026-04-18 09:45:17 +0545

    feat: restructure home page and add carousel component with data

---
+++ lib/carouselData.ts
@@ -0,0 +1,5 @@
+export const slides = [
+    {
+        image: "/about-banner.png"
+    }
+]
---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Carousel from '@/src/components/Carousel'
+
+const page = () => {
+  return (
+    <>
+      <Carousel />
+    </>
+  )
+}
+
+export default page
