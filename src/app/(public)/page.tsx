9c68e7b91e572f9db16b24806b04412558854f03
Parvesh <arveshchaudhary@gmail.com>
2026-04-18 10:32:31 +0545

    feat: add Facilities page and  icon in Facilities section

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Carousel from '@/src/components/Carousel'
 import About from '@/src/components/About'
 import RoomsSection from '@/src/components/Rooms'
+import FacilitiesSection from '@/src/components/Facilities'
 
 const page = () => {
   return (
     <>
       <Carousel />
       <About />
+      <FacilitiesSection />
       <RoomsSection />
     </>
   )
