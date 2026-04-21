e5bdc8698c9cf7c1600d7a56277818ae258d252a
Parvesh <arveshchaudhary@gmail.com>
2026-04-21 11:01:25 +0545

    refactor(next.config.ts): updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+
+      {
+        protocol: "http",
+        hostname: "api.hotel.hamroyouthit.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "api.hotel.hamroyouthit.com",
+        pathname: "/**",
+      },
+    ],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://api.hotel.hamroyouthit.com/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
